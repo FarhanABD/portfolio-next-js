c7279a9ec73a486fbe1c6d2677306f467863c4a2
FarhanABD <farhanabdilah204@gmail.com>
2026-07-04 17:30:19 +0700

    fix

--- types/project.d.ts
+++ types/project.d.ts
@@ -1,10 +1,12 @@
+import { ReactNode } from "react";
+
 export interface Project {
   title: string;
   description: string;
   tech: string[];
   github: string;
   demo: string;
-  icon: string;
+  icon: ReactNode;
   featured?: boolean;
   image: string;
 }
